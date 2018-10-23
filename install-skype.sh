3ca8bf9daea4d3d5e428af6f06a5d6235e53ed2e
Dhananjay <dhananjaymohnot@gmail.com>
2018-10-23 17:19:17 +0530

    install-skype

---
+++ install-skype.sh
@@ -0,0 +1,4 @@
+curl https://repo.skype.com/data/SKYPE-GPG-KEY | sudo apt-key add -
+echo "deb https://repo.skype.com/deb stable main" | sudo tee /etc/apt/sources.list.d/skypeforlinux.list
+sudo apt-get update
+sudo apt-get install skypeforlinux
